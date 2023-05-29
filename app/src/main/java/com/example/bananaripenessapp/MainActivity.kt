2905a9bb51cb057d9f07b42d4c35724f4fff1ce9
naromu <natyrm15173444@gmail.com>
2023-05-29 00:24:40 -0500

    Agregada la vista

--- app/src/main/java/com/example/bananaripenessapp/MainActivity.kt
+++ app/src/main/java/com/example/bananaripenessapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.bananaripenessapp
 
 import android.util.Base64
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
@@ -15,7 +14,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,13 +25,28 @@ import org.json.JSONObject
 import java.io.IOException
 import android.os.Environment
 import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
@@ -69,10 +82,10 @@ class MainActivity : ComponentActivity() {
         }
 
 
-    @SuppressLint("SimpleDateFormat")
+
     private fun createImageFile(): File {
         // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefix */
@@ -84,53 +97,31 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         imageUri = mutableStateOf(Uri.EMPTY)
 
         setContent {
-            Box(modifier = Modifier.fillMaxSize()) {
-                if (imageUri.value != Uri.EMPTY) {
-                    Image(
-                        painter = rememberImagePainter(data = imageUri.value),
-                        contentDescription = "Imagen capturada",
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop // opcional, para recortar la imagen si es necesario
-                    )
-                }
-
-                Text(dataState.value, modifier = Modifier.align(Alignment.TopCenter))
-                Text(mensajeState.value, modifier = Modifier.align(Alignment.Center))
-                Text(precioState.value, modifier = Modifier.align(Alignment.BottomCenter))
-
-
-                Button(
-                    onClick = {
-                        when {
-                            ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) ==
-                                    PackageManager.PERMISSION_GRANTED -> {
-                                // Ya tienes el permiso, realiza la acción que lo requiere
-                                val photoFile = createImageFile()
-                                val photoURI = FileProvider.getUriForFile(
-                                    this@MainActivity,
-                                    "com.example.bananaripenessapp.fileprovider",
-                                    photoFile
-                                )
-                                takePicture.launch(photoURI)
-                            }
-                            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
-                                // Explica al usuario por qué necesitas el permiso, y luego solicítalo.
-                            }
-                            else -> {
-                                // No tienes el permiso y no necesitas explicar por qué lo necesitas; solicítalo directamente
-                                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-                            }
-                        }
-                    },
-                    modifier = Modifier.align(Alignment.BottomCenter)
-                ) {
-                    Text("Tomar foto")
+            App(dataState, mensajeState, precioState, imageUri) {
+                when {
+                    ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_GRANTED -> {
+                        // Ya tienes el permiso, realiza la acción que lo requiere
+                        val photoFile = createImageFile()
+                        val photoURI = FileProvider.getUriForFile(
+                            this@MainActivity,
+                            "com.example.bananaripenessapp.fileprovider",
+                            photoFile
+                        )
+                        takePicture.launch(photoURI)
+                    }
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                        // Explica al usuario por qué necesitas el permiso, y luego solicítalo.
+                    }
+                    else -> {
+                        // No tienes el permiso y no necesitas explicar por qué lo necesitas; solicítalo directamente
+                        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    }
                 }
             }
         }
@@ -180,3 +171,69 @@ class MainActivity : ComponentActivity() {
         })
     }
 }
+
+@Preview
+@Composable
+fun App(    dataState: MutableState<String> = mutableStateOf(""),
+            mensajeState: MutableState<String> = mutableStateOf(""),
+            precioState: MutableState<String> = mutableStateOf(""),
+            imageUri: MutableState<Uri> = mutableStateOf(Uri.EMPTY),
+            onButtonClick: () -> Unit = {}){
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+        .padding(16.dp)){ // Agregado padding a todos los elementos en el LazyColumn
+        item{
+            Text(text = "Banana Ripeness Classifier",
+                modifier = Modifier
+                    .fillMaxWidth().fillMaxSize()
+                    .padding(top = 16.dp, bottom = 16.dp), // Agregado padding superior e inferior al texto
+                textAlign = TextAlign.Center,
+                fontSize = 32.sp
+            )
+            if (imageUri.value != Uri.EMPTY) {
+                Image(
+                    painter = rememberImagePainter(data = imageUri.value),
+                    contentDescription = "Imagen capturada",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(10.dp)) // Bordes redondeados
+                        .shadow(5.dp), // Sombra
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Image(painter = painterResource(id = R.drawable.bananaico),
+                    contentDescription = "Bananas",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(10.dp)) // Bordes redondeados
+                        .shadow(5.dp), // Sombra
+                )
+            }
+
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), // Agregado padding a la columna
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Text(dataState.value, modifier = Modifier.padding(top = 16.dp),fontSize = 28.sp)
+                Text(mensajeState.value, modifier = Modifier.padding(top = 16.dp))
+                Text(precioState.value, modifier = Modifier.padding(top = 16.dp))
+
+                Button(
+                    onClick = { onButtonClick() },
+                    modifier = Modifier.padding(top = 32.dp)
+                ) {
+                    Text("Tomar foto")
+                }
+            }
+        }
+    }
+}
+
+
+
